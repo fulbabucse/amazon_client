f47926d7b9e6eb646031a3af50d807631a200a43
Fulbabu <fulbabucse@gmail.com>
2023-02-11 20:05:37 +0600

    large device navbar design complete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Blogs from "../pages/Blogs";
 import Cart from "../pages/Cart";
+import Compare from "../pages/Compare";
 import Contact from "../pages/Contact";
 import Favorite from "../pages/Favorite";
 import Home from "../pages/Home";
 import OurShop from "../pages/OurShop";
+import SignIn from "../pages/users/SignIn";
+import SignUp from "../pages/users/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -16,8 +19,11 @@ const router = createBrowserRouter([
       { path: "/our-shop", element: <OurShop /> },
       { path: "/blogs", element: <Blogs /> },
       { path: "/contact", element: <Contact /> },
-      { path: "/favorite", element: <Favorite /> },
-      { path: "cart", element: <Cart /> },
+      { path: "/compare", element: <Compare /> },
+      { path: "/favorite-wishlist", element: <Favorite /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
     ],
   },
 ]);
