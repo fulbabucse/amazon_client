aa3975f138db9ecf354c01f5ac7378620de60f27
Fulbabu <fulbabucse@gmail.com>
2023-02-13 14:38:47 +0600

    reused carousel

--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -3,10 +3,12 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import { AiOutlineStar } from "react-icons/ai";
 import { MdFavoriteBorder } from "react-icons/md";
 import { Link } from "react-router-dom";
-import Carousel from "./Carousel";
+import { useState } from "react";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 const ProductCard = ({ product }) => {
   const { _id, title, price, rating, brand, images } = product;
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const ratingStar = Array.from({ length: 5 }, (_, i) => {
     let number = i + 0.5;
@@ -24,6 +26,21 @@ const ProductCard = ({ product }) => {
     );
   });
 
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSLide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
     <div className="bg-white rounded-md relative group h-full">
       <button className="absolute text-[#C9563C] font-normal top-2 right-2 hidden group-hover:block duration-300 ease-in-out">
@@ -35,17 +52,42 @@ const ProductCard = ({ product }) => {
         </div>
       )}
 
-      <div className="max-w-lg m-auto overflow-hidden w-full">
-        <Carousel>
-          {images?.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={title}
-              className="h-[250px] rounded-t-md w-full"
-            />
-          ))}
-        </Carousel>
+      <div className="max-w-[640px] h-[250px] w-full m-auto relative group">
+        <div className="w-full h-full rounded-t-md bg-center bg-cover duration-500">
+          <img
+            src={images[currentIndex]}
+            alt={title}
+            className="h-full transition-transform ease-out duration-500"
+          />
+        </div>
+
+        <div className="absolute inset-0 flex items-center justify-between p-3">
+          <button
+            onClick={prevSlide}
+            className="p-1 rounded-full shadow bg-white/80 text-primary hover:bg-white hidden group-hover:block cursor-pointer"
+          >
+            <BsChevronLeft size={15} />
+          </button>
+          <button
+            onClick={nextSlide}
+            className="p-1 rounded-full shadow bg-white/80 text-primary hover:bg-white hidden group-hover:block cursor-pointer"
+          >
+            <BsChevronRight size={15} />
+          </button>
+        </div>
+
+        <div className="absolute bottom-3 right-0 left-0 hidden group-hover:block">
+          <div className="flex items-center justify-center gap-1">
+            {images?.map((_, index) => (
+              <button
+                onClick={() => goToSLide(index)}
+                className={`transition-all w-2 h-2 bg-[#C9563C] rounded-full ${
+                  currentIndex === index ? "p-1" : "bg-opacity-50"
+                }`}
+              ></button>
+            ))}
+          </div>
+        </div>
       </div>
 
       <Link to={`/product/${_id}`}>
